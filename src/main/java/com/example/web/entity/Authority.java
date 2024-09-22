44550f70ee272592a8753ba1979540a1b9d104cc
luong <luongthptnqb123@gmail.com>
2024-09-22 23:07:03 +0700

    fix buff

--- src/main/java/com/example/web/entity/Authority.java
+++ src/main/java/com/example/web/entity/Authority.java
@@ -1,15 +1,15 @@
 package com.example.web.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
